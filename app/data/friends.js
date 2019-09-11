ac7d410790d5a985a46adc39f9effd251e9848ff
Eliza Irwin <elizairwin@gmail.com>
2019-09-11 10:32:40 -0400

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,114 @@
-//example: 
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
-  
+let friendsArray = [{
+    "name": "Lady Gaga",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Post Malone",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        4,
+        2,
+        3,
+        1,
+        2,
+        2,
+        4,
+        5,
+        5,
+        1
+    ]
+},
+{
+    "name": "Beyonce",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        2,
+        3,
+        1,
+        4,
+        4,
+        1,
+        1,
+        2,
+        5,
+        2
+    ]
+},
+{
+    "name": "John Legend",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        1,
+        1,
+        3,
+        2,
+        3,
+        4,
+        5,
+        5,
+        2,
+        1
+    ]
+},
+{
+    "name": "Jennifer Lawrence",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        4,
+        2,
+        1,
+        2,
+        3,
+        2,
+        4,
+        5,
+        1,
+        2
+    ]
+},
+{
+    "name": "Jason Bateman",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        5,
+        2,
+        4,
+        5,
+        1,
+        4,
+        3,
+        2,
+        4,
+        1
+    ]
+},
+{
+    "name": "Mindy Kaling",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        3,
+        5,
+        1,
+        5,
+        2,
+        3,
+        5,
+        4,
+        3,
+        3
+    ]
+}
+]
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,6 @@
 //get route with url api/friends
 //post route with /api/friends
+
+app.get("/api/friends", function(req, res) {
+    res.send("Welcome to Friend Finder!");
+  });
